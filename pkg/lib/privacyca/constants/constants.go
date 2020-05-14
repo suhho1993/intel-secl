e537ee6cb7c2995ea0bb76a6c1b021c809e1e8cd
R, Praveenraj <praveenraj.r@intel.com>
2020-05-14 05:46:42 -0700

    V3.0/task/privacyca process identity request

---
+++ pkg/lib/privacyca/constants/constants.go
@@ -0,0 +1,16 @@
+package constants
+
+const (
+	TPM2AlgorithmSymmetricAES = "AES"
+	SymmetricKeyBits128       = 128
+	TPM_ALG_AES               = 0x6
+	TPM_ES_NONE               = 0x1
+	SHORT_BYTES               = 2
+	TPM_ALG_RSA               = 1
+	TPM_SS_NONE               = 1
+	TPM_ES_SYM_CBC_PKCS5PAD   = 255
+	IDENTITY                  = "IDENTITY"
+	STORAGE                   = "STORAGE"
+	INTEGRITY                 = "INTEGRITY"
+	TPM_ALG_ID_SHA256         = 0x000B
+)
