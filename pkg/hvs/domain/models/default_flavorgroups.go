961f4f57b16d231f601febb25aa7e31ec9716451
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2020-06-01 22:47:43 +0530

    update enums

---
+++ pkg/hvs/domain/models/default_flavorgroups.go
@@ -0,0 +1,18 @@
+/*
+* Copyright (C) 2020 Intel Corporation
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+package models
+
+type FlavorGroups string
+
+const (
+	FlavorGroupsAutomatic        FlavorGroups = "automatic"
+	FlavorGroupsHostUnique       FlavorGroups = "host_unique"
+	FlavorGroupsPlatformSoftware FlavorGroups = "platform_software"
+	FlavorGroupsWorkloadSoftware FlavorGroups = "workload_software"
+)
+
+func (dfg FlavorGroups) String() string {
+	return dfg.String()
+}
