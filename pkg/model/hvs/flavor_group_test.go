5afbf9a4bc84d62bf6b94de80f18fdebd8961208
Harshitha <harshitha.m.gowda@intel.com>
2020-07-15 09:57:21 -0700

    added flavorgroup unit test to get match policy maps

--- pkg/model/hvs/flavor_group_test.go
+++ pkg/model/hvs/flavor_group_test.go
@@ -11,36 +11,43 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-const (
-	flavorgroupJson string = `{
-								"name": "hvs_flavorgroup_test3",
-								"flavor_match_policy_collection": {
-									"flavor_match_policies": [
-										{
-											"flavor_part": "PLATFORM",
-											"match_policy": {
-												"match_type": "ANY_OF",
-												"required": "REQUIRED"
-											}
-										},
-										{
-											"flavor_part": "OS",
-											"match_policy": {
-												"match_type": "ANY_OF",
-												"required": "REQUIRED"
-											}
-										},
-										{
-											"flavor_part": "HOST_UNIQUE",
-											"match_policy": {
-												"match_type": "LATEST",
-												"required": "REQUIRED_IF_DEFINED"
-											}
-										}
-									]
-								}
-							}`
-)
+var flavorgroupJson = `{
+   "id":"ee37c360-7eae-4250-a677-6ee12adce8e2",
+   "name":"hvs_flavorgroup_test3",
+   "flavor_match_policy_collection":{
+      "flavor_match_policies":[
+         {
+            "flavor_part":"PLATFORM",
+            "match_policy":{
+               "match_type":"ANY_OF",
+               "required":"REQUIRED"
+            }
+         },
+         {
+            "flavor_part":"OS",
+            "match_policy":{
+               "match_type":"ANY_OF",
+               "required":"REQUIRED"
+            }
+         },
+         {
+            "flavor_part":"HOST_UNIQUE",
+            "match_policy":{
+               "match_type":"LATEST",
+               "required":"REQUIRED_IF_DEFINED"
+            }
+         },
+         {
+            "flavor_part":"SOFTWARE",
+            "match_policy":{
+               "match_type":"ALL_OF",
+               "required":"REQUIRED"
+            }
+         }
+      ]
+   }
+}`
+
 
 var _ = Describe("FlavorGroup", func() {
 
@@ -51,6 +58,20 @@ var _ = Describe("FlavorGroup", func() {
 				var fg hvs.FlavorGroup
 				err := json.Unmarshal([]byte(flavorgroupJson), &fg)
 				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("get match policy types map for each flavorgroup", func() {
+		Context("Provided a valid Flavorgroup and get match policy details", func() {
+			It("Should generate valid maps", func() {
+				var fg hvs.FlavorGroup
+				err := json.Unmarshal([]byte(flavorgroupJson), &fg)
+				Expect(err).NotTo(HaveOccurred())
+				fpMap, mtMap, plcyMap := fg.GetMatchPolicyMaps()
+				Expect(len(fpMap)).Should(Equal(4))
+				Expect(len(mtMap)).Should(Equal(3))
+				Expect(len(plcyMap)).Should(Equal(2))
 			})
 		})
 	})
