f0011279a2b7f0696d9601efcf6cd1a2938f1118
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2020-07-28 10:28:20 +0530

    add TAG_ prefix for tags

--- pkg/hvs/services/hosttrust/saml_report.go
+++ pkg/hvs/services/hosttrust/saml_report.go
@@ -137,11 +137,12 @@ func getTags(trustReport *hvs.TrustReport) map[string]string {
 	defaultLog.Trace("hosttrust/saml_report:getTags() Entering")
 	defer defaultLog.Trace("hosttrust/saml_report:getTags() Leaving")
 
+	tagPrefix := "TAG_"
 	tagsMap := make(map[string]string)
 	for _, result := range trustReport.GetResultsForMarker(common.FlavorPartAssetTag.String()) {
 		if result.Rule.Name == faultsConst.RuleAssetTagMatches && len(result.Rule.Tags) > 0 {
 			for _, attribute := range result.Rule.Tags {
-				tagsMap[attribute.Key] = attribute.Value
+				tagsMap[tagPrefix + attribute.Key] = attribute.Value
 			}
 		}
 	}
