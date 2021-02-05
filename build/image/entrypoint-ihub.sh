dcd592dd8df86591fbe81651a7a2deee5308ac84
R, Praveenraj <praveenraj.r@intel.com>
2021-02-05 10:08:54 +0530

    cleanup directories in ihub container

--- build/image/entrypoint-ihub.sh
+++ build/image/entrypoint-ihub.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-SERVICE_USERNAME=ihub
-PRODUCT_HOME=/opt/$SERVICE_USERNAME
-BIN_PATH=$PRODUCT_HOME/bin
-LOG_PATH=/var/log/$SERVICE_USERNAME/
-CONFIG_PATH=/etc/$SERVICE_USERNAME/
+SERVICE=ihub
+CONFIG_PATH=/etc/ihub
+LOG_PATH=/var/log/$SERVICE/
 CERTS_PATH=$CONFIG_PATH/certs
 CERTDIR_TRUSTEDJWTCAS=$CERTS_PATH/trustedca
 SAML_CERT_DIR_PATH=$CERTS_PATH/saml
 
 if [ ! -f $CONFIG_PATH/.setup_done ]; then
-  for directory in $PRODUCT_HOME $BIN_PATH $LOG_PATH $CONFIG_PATH $CERTS_PATH $CERTDIR_TRUSTEDJWTCAS $SAML_CERT_DIR_PATH; do
+  for directory in $LOG_PATH $CONFIG_PATH $CERTS_PATH $CERTDIR_TRUSTEDJWTCAS $SAML_CERT_DIR_PATH; do
     mkdir -p $directory
     if [ $? -ne 0 ]; then
       echo "Cannot create directory: $directory"
